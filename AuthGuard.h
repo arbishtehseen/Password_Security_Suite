89a638931b8896966cf04d04303e6682a7b8af44
Arbish Randhawah <arbishtehseen20@gmail.com>
2026-05-12 19:09:33 +0500

    Update AuthGuard.h

--- AuthGuard.h
+++ AuthGuard.h
@@ -1,37 +1,36 @@
 #ifndef AuthGuard_h
 #define AuthGuard_h
-#include<string>
-#include<ctime>
+#include <string>
+#include <ctime>
 
-class AuthGuard{
-	
-	private:
-		// private member variables
-		std::string masterHash;
-		int failCount;
-		bool isLocked;
-		time_t lockTime;
-		
-		// private helper functions that shouldn't be accessed publically.
-		// convert to Hash
-	    std::string convertToHash(std::string masterpassword);
-	    void startLockTimer();
-		
-	public:
-		
-		//Default constructor
-		AuthGuard();
-		
-		//Parameterised Constructor
-		AuthGuard(std::string masterpassword);
-	
-	    // verify password
-	    bool verifymasterPassword(std::string masterpassword) ;
-	    bool getLockstatus() const;
-	    void resetfailCount();
-	    int getRemainingTime();
+class AuthGuard {
 
+private:
+    std::string masterHash;
+    int failCount;
+    bool isLocked;
+    time_t lockTime;
+
+    std::string convertToHash(std::string masterpassword);
+    void startLockTimer();
+
+    // Encrypts/decrypts the hash string before writing to disk
+    std::string encryptHash(std::string hash) const;
+
+public:
+    AuthGuard();
+    AuthGuard(std::string masterpassword);
+
+    // Save the encrypted hash to a .key file
+    bool saveHashToFile(const std::string& keyFilePath) const;
+
+    // Load and decrypt the hash from a .key file (replaces in-memory hash)
+    bool loadHashFromFile(const std::string& keyFilePath);
+
+    bool verifymasterPassword(std::string masterpassword);
+    bool getLockstatus() const;
+    void resetfailCount();
+    int  getRemainingTime();
 };
-
 
 #endif
